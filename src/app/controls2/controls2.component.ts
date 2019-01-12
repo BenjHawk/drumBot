09c38194c1c202320952b0654085a8c0c87086a7
Benjamin <benj.hauck@gmail.com>
2019-01-12 17:16:27 +0100

    added dataService (without testing)

--- src/app/controls2/controls2.component.ts
+++ src/app/controls2/controls2.component.ts
@@ -19,21 +19,17 @@ export class Controls2Component implements OnInit {
 
   ngOnInit() {
   }
-/*
   private clickedBtn(btnName: String): void {
     if(btnName === this.name[0]){
-      // dirty testing-method
       console.log(this.loopSvc.toDB());
     }
     if(btnName === this.name[1]){
-      // overwriting CUT functionality with pseudo-load!
       // dirty testing-method
       this.loopSvc.fromDB(
         ["[[false,false,true,false,true,true,true,false],[false,false,true,false,true,false,false,true],[true,true,true,false,true,true,true,false],[true,false,true,false,true,false,false,true],[true,true,true,false,true,true,true,true],[false,false,false,false,false,false,false,false]]"]
-      );
-    }
-    if(btnName === this.name[2])
+        );
+      }
+      if(btnName === this.name[2])
       this.loopSvc.play();
-  }
-*/
+    }
 }
